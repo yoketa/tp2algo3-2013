c22654e0a81c244582482ae50f33443ac84c5008
lucianalacroix <lucianalacroix@d87efc9a-4f5c-b422-39fd-3937909c260c>
2013-11-18 17:45:22 +0000

    Test de Vehiculo

--- test/fiuba/algo3/gpschallenge/VehiculoTest.java
+++ test/fiuba/algo3/gpschallenge/VehiculoTest.java
@@ -1,11 +1,15 @@
 package fiuba.algo3.gpschallenge;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import junit.framework.Assert;
 
 import org.junit.Test;
 
+import fiuba.algo3.gpschallenge.modelo.Auto;
+import fiuba.algo3.gpschallenge.modelo.CuatroXCuatro;
 import fiuba.algo3.gpschallenge.modelo.Moto;
+import fiuba.algo3.gpschallenge.modelo.Vector;
 import fiuba.algo3.gpschallenge.modelo.Vehiculo;
 
 public class VehiculoTest {
@@ -25,11 +29,26 @@ public class VehiculoTest {
 	}
 	
 	@Test
-	public void testDeberiaCrearUnVehiculoEnLaPosicionVerticalIndicada() {
-		int y  = 1;
-		Vehiculo vehiculo = Vehiculo.crearConPiloto("","",0,y);
-		Assert.assertEquals( y , vehiculo.getPosicionVertical() );
+	public void testDeberiaCrearUnVehiculoConElEstadoDeVehiculo_Moto() {
+		Moto moto = new Moto();
+		Vehiculo vehiculo = Vehiculo.crearConPiloto("","Moto",0,0);
+		assertEquals(moto.getClass(), vehiculo.getEstado().getClass());
 	}
+	
+	@Test
+	public void testDeberiaCrearUnVehiculoConElEstadoDeVehiculo_Auto() {
+		Auto auto = new Auto();
+		Vehiculo vehiculo = Vehiculo.crearConPiloto("","Auto",0,0);
+		assertEquals(auto.getClass(), vehiculo.getEstado().getClass());
+	}
+	
+	@Test
+	public void testDeberiaCrearUnVehiculoConElEstadoDeVehiculo_CuatroXCuatro() {
+		CuatroXCuatro cuatroXcuatro = new CuatroXCuatro();
+		Vehiculo vehiculo = Vehiculo.crearConPiloto("","CuatroXCuatro",0,0);
+		assertEquals(cuatroXcuatro.getClass(), vehiculo.getEstado().getClass());
+	}
+	
 	
 	@Test
 	public void testDeberiaCrearUnVehiculoConElEstadoDeVehiculoIndicado() {
@@ -44,5 +63,50 @@ public class VehiculoTest {
 		Vehiculo vehiculo = Vehiculo.crearConPiloto("","",0,0);
 		Assert.assertEquals( puntaje , vehiculo.getPuntaje());
 	}
+
+
+@Test
+public void testMoverDeberiaCambiarLaPosicionEnLaDireccionHorizontal() {
+
+	Vector direccion = new Vector(1,0);
+	Vehiculo vehiculo = Vehiculo.crearConPiloto("","",0,0);
+	
+	try {
+		vehiculo.mover(direccion);
+		Assert.assertEquals( 1 , vehiculo.getPosicionHorizontal() );
+		Assert.assertEquals( 0 , vehiculo.getPosicionVertical() );
+	}
+	catch (Exception e) {
+	}
+}
+
+@Test
+public void testMoverDeberiaCambiarLaPosicionEnLaDireccionVertical() {
+
+	Vector direccion = new Vector(0,1);
+	Vehiculo vehiculo = Vehiculo.crearConPiloto("","",0,0);
+	
+	try {
+		vehiculo.mover(direccion);
+		Assert.assertEquals( 1 , vehiculo.getPosicionVertical() );
+		Assert.assertEquals( 0 , vehiculo.getPosicionHorizontal() );
+	}
+	catch (Exception e) {
+	}
+}
+
+@Test
+public void testMoverDeberiaLanzarUnaExcepcionSiLaDireccionDeMovimientoNoEsOrtonormal() {
+
+	Vector direccion = new Vector(1,1);
+	Vehiculo vehiculo = Vehiculo.crearConPiloto("","",0,0);
+	
+	try {
+		vehiculo.mover(direccion);
+		fail();
+	}
+	catch (Exception e) {
+	}
+}
 }
 	
