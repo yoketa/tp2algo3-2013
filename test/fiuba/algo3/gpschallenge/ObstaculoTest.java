3ff12820d5d803c126fb1d5cf6c1a5b9cc0fc695
ramoncenturion74 <ramoncenturion74@gmail.com>
2013-11-20 16:02:47 +0000

    Agregue un constructor para vehiculo que creo que es mejor, necesito que lo chequeen y me digan si va ese y borro el resto, modifico los test donde romperia dsp. Deje control Policial sin probar en obstaculoTest porque nose como usa la probabilidad.

--- test/fiuba/algo3/gpschallenge/ObstaculoTest.java
+++ test/fiuba/algo3/gpschallenge/ObstaculoTest.java
@@ -1,19 +1,16 @@
 package fiuba.algo3.gpschallenge;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.Test;
 
-import fiuba.algo3.gpschallenge.modelo.CambioDeVehiculo;
-import fiuba.algo3.gpschallenge.modelo.CuatroXCuatro;
 import fiuba.algo3.gpschallenge.modelo.EstadoVehiculo;
 import fiuba.algo3.gpschallenge.modelo.Moto;
 import fiuba.algo3.gpschallenge.modelo.Obstaculo;
 import fiuba.algo3.gpschallenge.modelo.Piquete;
 import fiuba.algo3.gpschallenge.modelo.Pozo;
-import fiuba.algo3.gpschallenge.modelo.Sorpresa;
-import fiuba.algo3.gpschallenge.modelo.SorpresaDesfavorable;
-import fiuba.algo3.gpschallenge.modelo.SorpresaFavorable;
+import fiuba.algo3.gpschallenge.modelo.Probabilidad;
 import fiuba.algo3.gpschallenge.modelo.Vector;
 import fiuba.algo3.gpschallenge.modelo.Vehiculo;
 
@@ -48,58 +45,20 @@ public class ObstaculoTest {
 			fail();
 		}	
 	}
-//
+
 //	@Test
-//	public void testCambioDeVehiculoDebeImplementarEvento() {
-//		Sorpresa sorpresa = new CambioDeVehiculo();
-//		Vehiculo vehiculo = Vehiculo.crearConPiloto("", "Auto", 0, 0);
+//	public void testControlPolicialDebeImplementarObstaculo() {
+//		Obstaculo obstaculo = new ControlPolicial(probabilidad);
+//		EstadoVehiculo estadoMoto = new Moto();
+//		Vehiculo vehiculo = Vehiculo.crearConPiloto("",estadoMoto,0,0);
 //		
 //		try {
-//			sorpresa.afectar(vehiculo);
+//			obstaculo.afectar(vehiculo);
 //			assertTrue(true);
 //		}
 //		catch (Exception ex) {
 //			fail();
 //		}	
 //	}
-//	
-//	@Test
-//	public void testSorpresaDesfavorableSumaVeinticincoPorciento() {
-//		Sorpresa sorpresa = new SorpresaDesfavorable();
-//		Vehiculo vehiculo = Vehiculo.crearConPiloto("", "Auto", 0, 0);
-//		
-//		try {
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			
-//			sorpresa.afectar(vehiculo);
-//			
-//			assertEquals(5*1.25, vehiculo.getPuntaje(), 0.01);
-//		} catch (Exception e) {
-//			fail();
-//		}
-//	}
-//	
-//	@Test
-//	public void testSorpresaFavorableRestaVeintePorciento() {
-//		Sorpresa sorpresa = new SorpresaFavorable();
-//		Vehiculo vehiculo = Vehiculo.crearConPiloto("", "Auto", 0, 0);
-//		
-//		try {
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			vehiculo.mover(new Vector(1,0));
-//			
-//			sorpresa.afectar(vehiculo);
-//			
-//			assertEquals(5*0.8, vehiculo.getPuntaje(), 0.01);
-//		} catch (Exception e) {
-//			fail();
-//		}
-//	}
+
 }
