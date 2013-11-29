1915b0b7cbfd2d1d70636acbf4b627c47a5f8b76
ramoncenturion74 <ramoncenturion74@gmail.com>
2013-11-29 04:58:14 +0000

    Primera parte de Interfaz, solo es estructura, los botones andan pero solo viajan por los paneles, menos en uno. cuando tengasn todo listo para guardar los usuarios y poder cargarlos avisen y lo conecto

--- fiuba.algo3.gpschallenge/Principal/Main.java
+++ fiuba.algo3.gpschallenge/Principal/Main.java
@@ -1,5 +1,13 @@
 package Principal;
 
+import panel.PanelInicial;
+
 public class Main {
 
+    public static void main(String[] args) {
+        PanelInicial panelInicial = new PanelInicial();
+        panelInicial.setBounds(500,250,450,250);
+        panelInicial.setVisible(true);
+        
+    }
 }
---
+++ fiuba.algo3.gpschallenge/panel/MenuRegistracion.java
@@ -0,0 +1,96 @@
+package panel;
+
+
+/**
+ *
+ * @author Ramon
+ */
+public class MenuRegistracion extends javax.swing.JFrame {
+
+    /** Creates new form MenuRegistracion */
+    public PanelInicial menu;
+    
+    public MenuRegistracion() {
+        initComponents();
+    }
+
+    public MenuRegistracion(PanelInicial panel) {
+        initComponents();
+        menu = panel;
+        panel.setVisible(false);
+    }   
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">
+    private void initComponents() {
+
+        jLabel = new javax.swing.JLabel();
+        jBguardar = new javax.swing.JButton();
+        jUsuario = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel.setFont(new java.awt.Font("Consolas", 1, 24)); // NOI18N
+        jLabel.setText("Por favor elija un nombre");
+
+        jBguardar.setFont(new java.awt.Font("Comic Sans MS", 0, 18)); // NOI18N
+        jBguardar.setText("Guardar");
+        jBguardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBguardarActionPerformed(evt);
+            }
+        });
+
+        jUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jUsuarioActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(82, 82, 82)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jBguardar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jUsuario, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 74, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(52, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(jLabel)
+                .addGap(18, 18, 18)
+                .addComponent(jUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jBguardar, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>
+
+    private void jBguardarActionPerformed(java.awt.event.ActionEvent evt) {
+        menu.setVisible(true);
+        this.dispose();
+    }
+
+    private void jUsuarioActionPerformed(java.awt.event.ActionEvent evt) {
+
+    }
+
+
+    // Variables declaration - do not modify
+    private javax.swing.JButton jBguardar;
+    private javax.swing.JLabel jLabel;
+    private javax.swing.JTextField jUsuario;
+    // End of variables declaration
+}
