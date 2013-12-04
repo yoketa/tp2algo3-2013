de2e8c5c219bbbd0c5eb6df9f79037f2fbd6342f
nucleo86f <nucleo86f@gmail.com>
2013-12-04 02:39:39 +0000

    Agrego clase partida y su Test correspondiente

--- fiuba.algo3.gpschallenge/persistencia/Archivador.java
+++ fiuba.algo3.gpschallenge/persistencia/Archivador.java
@@ -2,6 +2,7 @@ package persistencia;
 
 import java.io.FileWriter;
 
+import modelo.juego.Partida;
 import modelo.juego.Ranking;
 import modelo.vehiculo.Vehiculo;
 
@@ -110,6 +111,37 @@ public class Archivador {
 	        Document document = builder.build(pathArchivo);
 	        
 	        return Nivel.cargarDesdeXML(document.getRootElement());
+	        
+		} catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void guardar(Partida nivel, String pathArchivo) {
+		try {
+	        
+			Element root = nivel.serializarXML();
+	        Document document = new Document(root);
+
+	        XMLOutputter outputter = new XMLOutputter();
+	        outputter.setFormat(Format.getPrettyFormat());
+	        
+	        FileWriter writer = new FileWriter(pathArchivo);
+	        outputter.output(document,writer);
+	        writer.close();
+	        
+	      } catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	public static Partida cargar(String pathArchivo) {
+		try {
+	        SAXBuilder builder = new SAXBuilder();
+	        Document document = builder.build(pathArchivo);
+	        
+	        return Partida.cargarDesdeXML(document.getRootElement());
 	        
 		} catch(Exception e) {
 			throw new RuntimeException(e);
