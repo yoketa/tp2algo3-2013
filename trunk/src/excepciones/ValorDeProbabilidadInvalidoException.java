e140c733c8032ff19bcf6e070e794a6d8da76c34
lucianalacroix <lucianalacroix@d87efc9a-4f5c-b422-39fd-3937909c260c>
2013-12-10 07:42:06 +0000

    Exception Proba

---
+++ trunk/src/excepciones/ValorDeProbabilidadInvalidoException.java
@@ -0,0 +1,15 @@
+package excepciones;
+
+	public class ValorDeProbabilidadInvalidoException extends Exception {
+		
+		/**
+		 * 
+		 */
+		
+		private static final long serialVersionUID = 1L;
+		
+		public ValorDeProbabilidadInvalidoException(String mensaje) {
+			super(mensaje);
+		}
+
+}
