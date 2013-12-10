4404ad0fcc60991d01f5680a8459655d01e4809d
lucianalacroix <lucianalacroix@d87efc9a-4f5c-b422-39fd-3937909c260c>
2013-12-10 18:04:41 +0000

    Probabilidad refactor y test

--- trunk/src/modelo/probabilidades/ProbabilidadFija.java
+++ trunk/src/modelo/probabilidades/ProbabilidadFija.java
@@ -1,15 +1,19 @@
 package modelo.probabilidades;
 
+import excepciones.ValorDeProbabilidadInvalidoException;
+
 
 public class ProbabilidadFija extends Probabilidad {
 	
-	public ProbabilidadFija(double valorFijo){
-
+	public ProbabilidadFija(double valorFijo) throws ValorDeProbabilidadInvalidoException{
+		
+		if ( valorFijo >= 0 && valorFijo <= 1){
 		 this.probabilidad = valorFijo;
+		 
+		}else throw new ValorDeProbabilidadInvalidoException("No es un valor de probabilidad");
+			
 	}
-	
 	public double calcular() {
 		return this.probabilidad;	
 	}
-
 }
--- trunk/test/Unitarias/ProbabilidadFijaTest.java
+++ trunk/test/Unitarias/ProbabilidadFijaTest.java
@@ -10,19 +10,30 @@ import org.junit.Test;
 public class ProbabilidadFijaTest {
 
 	@Test
-	public void testCalcularDeberiaRetornarElValorDeProbabilidadFijado() throws ValorDeProbabilidadInvalidoException {
+	public void testCalcularDeberiaRetornarElValorDeProbabilidadFijado() {
 		double x = 0.2;
 		
-		ProbabilidadFija probabilidadFija = new ProbabilidadFija(x);
-		Assert.assertEquals( x , probabilidadFija.calcular(), 0);		
+		try {
+			ProbabilidadFija probabilidadFija = new ProbabilidadFija(x);
+			Assert.assertEquals( x , probabilidadFija.calcular(), 0);
+		}
+		catch ( ValorDeProbabilidadInvalidoException ex) {
+			System.out.println(ex.toString());
+		}		
 	}
 	
 	@Test
 	public void testProbabilidadFijaDeberiaLanzarValorDeProbabilidadInvalidoExceptionSiIntentaCrearConUnValorInvalido() {
 		double x = 1.1;
 		
-		ProbabilidadFija probabilidadFija = new ProbabilidadFija(x);
-		Assert.assertEquals( x , probabilidadFija.calcular(), 0);		
+		try {
+			ProbabilidadFija probabilidadFija = new ProbabilidadFija(x);
+			Assert.assertEquals( x , probabilidadFija.calcular(), 0);
+		}
+		catch ( ValorDeProbabilidadInvalidoException ex) {
+			System.out.println(ex.toString());
+		}		
 	}
 }
+
 
